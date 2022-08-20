7f45a822a5f7664659f7a270d9acfa6e787deb6b
Raphael Paulino <raphael.paulino.web@gmail.com>
2022-08-20 20:39:50 -0300

    feat: creates the first endpoint list

---
+++ src/main/java/br/com/raphaelpaulino/parking/controller/mapper/ParkingMapper.java
@@ -0,0 +1,23 @@
+package br.com.raphaelpaulino.parking.controller.mapper;
+
+import br.com.raphaelpaulino.parking.controller.dto.ParkingDTO;
+import br.com.raphaelpaulino.parking.controller.model.Parking;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class ParkingMapper {
+
+    private static final ModelMapper MODEL_MAPPER = new ModelMapper();
+
+    public ParkingDTO parkingDTO(Parking parking) {
+        return MODEL_MAPPER.map(parking, ParkingDTO.class);
+    }
+
+    public List<ParkingDTO> toParkingDTOList(List<Parking> parkingList) {
+        return parkingList.stream().map(this::parkingDTO).collect(Collectors.toList());
+    }
+}
