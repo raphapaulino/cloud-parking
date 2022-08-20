c004c37c6c1097dc9e2678f5e0512b5c7293fb01
Raphael Paulino <raphael.paulino.web@gmail.com>
2022-08-20 17:32:42 -0300

    feat: initial structure

---
+++ src/main/java/br/com/raphaelpaulino/parking/ClougParkingApplication.java
@@ -0,0 +1,13 @@
+package br.com.raphaelpaulino.parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClougParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClougParkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/raphaelpaulino/parking/ClougParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.raphaelpaulino.parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClougParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
