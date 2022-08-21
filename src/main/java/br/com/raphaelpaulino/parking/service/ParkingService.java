83c9210a071b0e4f9b8018946f85dde9d6a00ce2
Raphael Paulino <raphael.paulino.web@gmail.com>
2022-08-20 21:52:56 -0300

    feat: add parking DTOs and parking mapper

--- src/main/java/br/com/raphaelpaulino/parking/service/ParkingService.java
+++ src/main/java/br/com/raphaelpaulino/parking/service/ParkingService.java
@@ -3,6 +3,7 @@ package br.com.raphaelpaulino.parking.service;
 import br.com.raphaelpaulino.parking.controller.model.Parking;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -13,16 +14,30 @@ public class ParkingService {
 
     static {
         var id = getUUID();
+        var id1 = getUUID();
         Parking parking = new Parking(id, "DMS-1111", "SC", "CELTA", "PRETO");
+        Parking parking1 = new Parking(id1, "WAS-2365", "SP", "VW GOL", "VERMELHO");
         parkingMap.put(id, parking);
+        parkingMap.put(id1, parking1);
     }
 
     public List<Parking> findAll() {
-//        return Arrays.asList();
         return parkingMap.values().stream().collect(Collectors.toList());
     }
 
     private static String getUUID() {
         return UUID.randomUUID().toString().replace("-", "");
+    }
+
+    public Parking findById(String id) {
+        return parkingMap.get(id);
+    }
+
+    public Parking create(Parking parkingCreate) {
+        String uuid = getUUID();
+        parkingCreate.setId(uuid);
+        parkingCreate.setEntryDate(LocalDateTime.now());
+        parkingMap.put(uuid, parkingCreate);
+        return parkingCreate;
     }
 }
